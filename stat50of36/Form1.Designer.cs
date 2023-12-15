7fb8031d9ec876332f834be8bf13b7cc49872c48
Dulakshana-VI <dulakshana@victoryinformation.biz>
2023-12-15 14:32:40 +0530

    First Commit

---
+++ stat50of36/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace stat50of36
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel1 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(213, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(95, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.CustomLabels.Add(customLabel1);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(57, 232);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "datapoints";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(416, 256);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(289, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 127);
+            this.label2.MaximumSize = new System.Drawing.Size(416, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(209, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Enter your value";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(325, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "And press SPACE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(531, 503);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Statistics";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ stat50of36/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace stat50of36
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<int> data = new List<int>();
+        private int nextint;
+        private Dictionary<int, int> dictionary;
+        private bool mac = false;
+        private string Decryptcode;
+        private void fillChart(int y)
+        {
+            chart1.Series["datapoints"].Points.Clear();
+            foreach (KeyValuePair<int, int> entry in dictionary)
+            {
+                chart1.Series["datapoints"].Points.AddXY(entry.Key, entry.Value);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.Parse(textBox1.Text) < 37)
+                {
+                    textBox1.ForeColor = Color.Green;
+                    nextint = int.Parse(textBox1.Text);
+                }
+                else
+                {
+                    textBox1.ForeColor = Color.Red;
+                    MessageBox.Show("You must enter a value between 1 - 36");
+                    textBox1.Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            dictionary = new Dictionary<int, int>(
+                Enumerable.Range(1, 36).ToDictionary(i => i, _ => 0)
+                );
+
+            //MessageBox.Show("MAC Address: " + macAddress);
+            //CustomBusyBox.BusyBox.ShowBusy();
+            string key = "no key";
+            try
+            {
+                key = ConfigurationManager.AppSettings["Dcode"];
+                Decryptcode = AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", key);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to read the key");
+                this.Close();
+            }
+            try
+            {
+                //string test = "1062E5E00299,0250F2F5ACE0,005056C00001,005056C00008,00FFBEF89159,00FF7EF1B0B9,";
+                //string encrypted = AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", test);
+                //MessageBox.Show(encrypted);
+                //MessageBox.Show(AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", encrypted));
+
+                //"9EL4TGcAJOuCgRa+mDrDrBJoBYDDXh8+tH8qjn+JIvPhSvWB0qyemXjPeBAS0z9xjgqs8ZtUKwTHZMz3xg76nQfZp7UwDzIWSeg7Fo99UW4=";
+
+                //Decryptcode = AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", "9EL4TGcAJOuCgRa+mDrDrBJoBYDDXh8+tH8qjn+JIvPhSvWB0qyemXjPeBAS0z9xjgqs8ZtUKwTHZMz3xg76nQfZp7UwDzIWSeg7Fo99UW4=");
+
+                checkmac(Decryptcode);
+                
+                if (mac)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("You have no right to use this software");
+                    this.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to read the key");
+                this.Close();
+            }
+            //CustomBusyBox.BusyBox.CloseBusy();
+        }
+
+        private void checkmac(string Decryptcode)
+        {
+            string macAddress = "";
+            //foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            //{
+            //    if (nic.OperationalStatus == OperationalStatus.Up)
+            //    {
+            //        macAddress += nic.GetPhysicalAddress().ToString() + " ";
+            //    }
+            //}
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    macAddress += nic.GetPhysicalAddress().ToString() + ",";
+                }
+            }
+            //MessageBox.Show(macAddress);
+            string[] macset = macAddress.Split(',');
+            string[] checkset = Decryptcode.Split(',');
+            foreach (string mac1 in macset)
+            {
+                foreach (string check1 in checkset)
+                {
+                    if (mac1 == check1)
+                    {
+                        mac = true;
+                    }
+                    else
+                    {
+                    }
+                }
+                
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Space)
+            {
+                try
+                {
+                    if (nextint == 0)
+                    {
+                        MessageBox.Show("You must enter a value between 1 - 36");
+                    }
+                    else if (data.Count < 50)
+                    {
+                        data.Add(nextint);
+                        dictionary[nextint]++;
+                    }
+                    else
+                    {
+
+                        dictionary[data[0]]--;
+                        dictionary[nextint]++;
+                        data.Remove(data[0]);
+                        data.Add(nextint);
+                    }
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("You must enter a value between 1 - 36");
+                }
+                label1.Text = data.Count.ToString();
+
+                string listString = "";
+
+                foreach (int number in data)
+                {
+                    listString += number + ", ";
+                }
+
+                label2.Text = listString;
+
+                fillChart(nextint);
+
+                textBox1.Clear();
+                nextint = 0;
+            }
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
